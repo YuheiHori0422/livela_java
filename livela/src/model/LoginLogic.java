2e7358f2857c20636c778ca8e8164617c8518f39
yuhei <yu.hori0010@linela.jp>
2019-07-29 16:02:42 +0900

    second-commit #1

--- livela/src/model/LoginLogic.java
+++ livela/src/model/LoginLogic.java
@@ -1,7 +1,7 @@
 package model;
 
 public class LoginLogic {
-	public boolean execute(UserBeans, userBeans) {
+	public boolean execute(UserBeans userBeans) {
 		return userBeans.getPass().equals("1234");
 	}
 }
